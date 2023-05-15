878e4833344722927edb6223086d100904759ef1
sahithi0204 <113923373+sahithi0204@users.noreply.github.com>
2023-05-15 10:51:09 +0530

    Update CNS EXP 16.py

--- CNS EXP 16.py
+++ CNS EXP 16.py
@@ -1,18 +1,60 @@
-pt=str(input("ENTER THE PLAIN TEXT : "))
-cipher=""
-letter="abcdefghijklmnopqrstuvwxyz"
-common=max(set(pt),key=pt.count)
-print("COMMON LETTER : "+common)
-if common in letter:
-    com=letter.find(common)
-key=com-6
-#print("key = "+common+" - g = "+key)
-if (key<0):
-    key=26-key
-for i in pt:
-    if i in letter:
-        pos=letter.find(i)
-        new_pos=(pos+key)%26
-        new_char=letter[new_pos]
-        cipher+=new_char
-print("CIPHER TEXT : "+cipher)
+import string
+
+
+def get_letter_frequencies(ciphertext):
+    frequencies = {letter: 0 for letter in string.ascii_lowercase}
+    total_letters = 0
+
+    for letter in ciphertext:
+        if letter.isalpha():
+            frequencies[letter.lower()] += 1
+            total_letters += 1
+
+    for letter in frequencies:
+        frequencies[letter] /= total_letters
+
+    return frequencies
+
+
+def decrypt(ciphertext, key):
+    plaintext = ""
+    for letter in ciphertext:
+        if letter.isalpha():
+            is_upper = letter.isupper()
+            letter = letter.lower()
+            decrypted_letter = chr((ord(letter) - ord('a') - key) % 26 + ord('a'))
+            if is_upper:
+                decrypted_letter = decrypted_letter.upper()
+            plaintext += decrypted_letter
+        else:
+            plaintext += letter
+    return plaintext
+
+
+def letter_frequency_attack(ciphertext, top_k=10):
+    frequencies = get_letter_frequencies(ciphertext)
+    sorted_frequencies = sorted(frequencies.items(), key=lambda x: x[1], reverse=True)
+    possible_plaintexts = []
+
+    for i in range(26):
+        key = i
+        plaintext = decrypt(ciphertext, key)
+        score = 0
+        for j, freq in enumerate(sorted_frequencies):
+            score += abs(ord(freq[0]) - ord(plaintext[j].lower()))
+        possible_plaintexts.append((score, plaintext))
+
+    possible_plaintexts.sort(key=lambda x: x[0])
+    return possible_plaintexts[:top_k]
+
+
+# Example usage
+ciphertext = "Jxu gwcujp xqq gr ejtkzrwp gz xwzpvk. T jgzp qttg fqwq cu yjcv."  # Additive cipher encrypted text
+top_k = 10
+
+results = letter_frequency_attack(ciphertext, top_k)
+
+print(f"Top {top_k} possible plaintexts:")
+for i, result in enumerate(results, 1):
+    score, plaintext = result
+    print(f"{i}. Plaintext: {plaintext}, Score: {score}")
